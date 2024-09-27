6a4dd178ab7bdfe5bccab7274ee3ce98027f8150
Mazen Tamer Salah <mazentamer3056@gmail.com>
2024-09-27 11:38:12 +0300

    Refactor User class to UserModel and add serialization methods

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,14 +1,14 @@
 import 'package:roomie_radar/models/room_review_model.dart';
 
-class User {
+class UserModel {
   final String id; // Unique identifier for the user
   final String name; // User's name
   final String email; // User's email
   final String profilePicture; // URL of the user's profile picture
-  final List<RoomReview> reviews; // List of reviews given by the user
+  final List<RoomReviewModel> reviews; // List of reviews given by the user
   final List<String> likedRooms; // List of room IDs the user likes
 
-  User({
+  UserModel({
     required this.id,
     required this.name,
     required this.email,
@@ -16,4 +16,30 @@ class User {
     this.reviews = const [],
     this.likedRooms = const [],
   });
+
+  // Convert user data to a map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'profilePicture': profilePicture,
+      'reviews': reviews.map((review) => review.toMap()).toList(),
+      'likedRooms': likedRooms,
+    };
+  }
+
+  // Create a user object from a map
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'],
+      name: map['name'],
+      email: map['email'],
+      profilePicture: map['profilePicture'],
+      reviews: List<RoomReviewModel>.from(
+        map['reviews'].map((review) => RoomReviewModel.fromMap(review)),
+      ),
+      likedRooms: List<String>.from(map['likedRooms']),
+    );
+  }
 }
