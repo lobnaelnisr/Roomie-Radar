ca7efa76320edf83ee3a872bc947df5b72db1756
Mazen Tamer Salah <mazentamer3056@gmail.com>
2024-09-27 11:30:26 +0300

    Add Compatibility model to represent user compatibility data

---
+++ lib/models/compatability_model.dart
@@ -0,0 +1,11 @@
+class Compatibility {
+  final String user1Id; // ID of the first user
+  final String user2Id; // ID of the second user
+  final double percentage; // Compatibility percentage
+
+  Compatibility({
+    required this.user1Id,
+    required this.user2Id,
+    required this.percentage,
+  });
+}
