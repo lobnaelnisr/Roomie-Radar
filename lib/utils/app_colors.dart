308981958e08cf0adf4710005c4501c7df347993
Mazen Tamer Salah <mazentamer3056@gmail.com>
2024-09-28 22:54:58 +0300

    Add app_colors and app_theme for updated color palette and theming

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+// Updated color palette
+const rfPrimaryColor = Color(0xFFFFA500); // Orange color
+const rfSecondaryColor = Color(0xFF808080); // Grey color
+const rfSplashBgColor = Color(0xFFFFE5B4); // Light orange color
+const rfCategoryBgColor = Color(0xFFF3F3F3);
+const rfSelectedCategoryBgColor = Color(0xFFD3D3D3); // Lighter grey color
+const rfFaqBgColor = Color(0xFFE6E6E6);
+const rfNotificationBgColor = Color(0xFFF2F2F2);
+const rfRattingBgColor = Color(0xFFFFA07A); // Light salmon color
+const rfTextColor = Color(0xFF4F4F4F);
+
+// Light Theme Colors with orange and grey palette
+const whiteColor = Colors.white;
+const viewLineColor = Color(0xFFDADADA);
+const textPrimaryColor = Color(0xFF4F4F4F);
+const textSecondaryColor = Color(0xFFA9A9A9);
+const blackColor = Colors.black;
+
+// Dark Theme Colors with orange and grey palette
+const appBackgroundColorDark = Color(0xFF2E2E2E); // Dark grey color
+const cardBackgroundBlackDark = Color(0xFF404040); // Slightly lighter dark grey
+const colorPrimaryBlack = Color(0xFF131d25);
+const appColorPrimaryDarkLight = Color(0xFFFF8C00); // Dark orange color
+const iconColorPrimaryDark = Color(0xFFF5F5F5);
+const iconColorSecondaryDark = Color(0xFFA9A9A9);
+const appShadowColorDark = Color(0x1A3E3942);
+const appColorPrimary = rfPrimaryColor;
+const appSecondaryBackgroundColor = Color(0xFF3D3D3D);
+const iconColorPrimary = Color(0xFFFFFFFF);
+const iconColorSecondary = Color(0xFFA8ABAD);
+const appColorPrimaryLight = Color(0xFFFFA500); // Same orange for consistency
+const appTextColorPrimary = Color(0xFF212121);
+const appTextColorSecondary = Color(0xFF5A5C5E);
+const appShadowColor = Color(0x95E9EBF0);
