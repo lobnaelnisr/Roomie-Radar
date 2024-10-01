d437c1c064f741f2a8170f3475f06f7615cbcb90
Marmar9999 <119695675+Marmar9999@users.noreply.github.com>
2024-10-01 05:02:14 +0300

    sign in & sign up

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:roomie_radar/firebase_options.dart';
 import 'package:roomie_radar/utils/app_theme.dart';
+import 'package:roomie_radar/views/authentication/signInScreen.dart';
+import 'package:roomie_radar/views/authentication/signUpScreen.dart';
 import 'package:roomie_radar/views/listings/room_listing.dart';
+// import 'package:roomie_radar/views/auth/signInScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
 
   runApp(const MainApp());
 }
@@ -18,9 +22,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       theme: AppThemeData.lightTheme,
-      home: const RoomListing(),
+      initialRoute: '/signUp',
+      routes: {
+        '/signIn': (context) => const SignInScreen(),
+        '/signUp': (context) => const SignUpScreen(),
+        '/roomListing': (context) => const RoomListing(),
+      },
     );
   }
 }
