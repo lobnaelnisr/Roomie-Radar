20145cd4946e609a02b656b22c96da39ae877e08
Mazen Tamer Salah <mazentamer3056@gmail.com>
2024-10-04 00:22:09 +0300

    Refactor user authentication logic into FirebaseUserService and define abstract UserRepository

---
+++ lib/services/firebase/firebase_user_service.dart
@@ -0,0 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:roomie_radar/models/user_model.dart';
+import 'package:roomie_radar/repositories/user_repository.dart';
+
+class FirebaseUserService extends UserRepository {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
+  Future<UserModel?> signUp(String email, String password, String name) async {
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      // Create user document in Firestore
+      UserModel user = UserModel(
+        id: userCredential.user!.uid,
+        name: name,
+        email: email,
+        profilePicture: "", // Default or placeholder
+      );
+      await _firestore.collection('users').doc(user.id).set(user.toMap());
+      return user;
+    } catch (e) {
+      throw Exception("Error signing up: $e");
+    }
+  }
+
+  @override
+  Future<UserModel?> signIn(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return await getUserById(userCredential.user!.uid);
+    } catch (e) {
+      throw Exception("Error signing in: $e");
+    }
+  }
+
+  @override
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  Future<UserModel?> getUserById(String id) async {
+    try {
+      DocumentSnapshot doc = await _firestore.collection('users').doc(id).get();
+      return UserModel.fromMap(doc.data() as Map<String, dynamic>);
+    } catch (e) {
+      throw Exception("Error fetching user: $e");
+    }
+  }
+}
