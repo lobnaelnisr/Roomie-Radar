8138d0e089f1f10757580dd8ceabcbd0f3934d2c
Mazen Tamer Salah <mazentamer3056@gmail.com>
2024-10-04 10:43:17 +0300

    Add CustomTextField widget for reusable text input with validation

---
+++ lib/views/authentication/components/custom_text_field.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final IconData icon;
+  final bool obscureText;
+  final String? Function(String?)? validator;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.labelText,
+    required this.icon,
+    this.obscureText = false,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        labelText: labelText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+        prefixIcon: Icon(icon, color: Colors.blue), // Customize icon color
+      ),
+      validator: validator,
+    );
+  }
+}
