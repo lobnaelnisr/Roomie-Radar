740caa4e06733ddd5fa4e6ee865544bb40b4aeae
Mazen Tamer Salah <mazentamer3056@gmail.com>
2024-10-11 03:15:25 +0300

    Refactor main.dart: update initial route logic for authentication and add QuestionnaireView

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:roomie_radar/firebase_options.dart';
 import 'package:roomie_radar/utils/app_theme.dart';
-import 'package:roomie_radar/views/authentication/signInScreen.dart';
-import 'package:roomie_radar/views/authentication/signUpScreen.dart';
 import 'package:roomie_radar/views/listings/room_listing.dart';
-
 import 'package:roomie_radar/views/authentication/sign_in_screen.dart';
 import 'package:roomie_radar/views/authentication/sign_up_screen.dart';
-
 import 'package:firebase_core/firebase_core.dart';
+import 'package:roomie_radar/views/questionnaire/qustionnaire_view.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,14 +24,17 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: AppThemeData.lightTheme,
-      initialRoute: '/signUp',
+      initialRoute: FirebaseAuth.instance.currentUser == null
+          ? '/signIn'
+          : '/roomListing',
       routes: {
         '/signIn': (context) => const SignInScreen(),
         '/signUp': (context) => const SignUpScreen(),
+        '/Questionnaire': (context) => const QuestionnaireView(),
         '/roomListing': (context) => const RoomListing(),
       },
-
     );
   }
 }
